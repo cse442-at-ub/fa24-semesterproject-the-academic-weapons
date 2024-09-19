398e24cd50847b03a1a683126691ee20af076762
Journey <journeys@buffalo.edu>
2024-09-18 22:52:08 -0400

    Renamed frontend folder... Also added app background, app routes, and some basic file structure folders.

---
+++ frontend/src/Page/Settings.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import Background from "../Component/Background";
+const Settings = () => {
+
+
+  return (
+    // <Background>
+        <div>
+            <h1>Settings</h1>
+            <div>Hello?</div>
+        </div>
+    // </Background>
+  );
+};
+
+export default Settings;
