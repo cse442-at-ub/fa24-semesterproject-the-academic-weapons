9f6f618228c29ad075f2bde6a3da96433b4000cf
Journey <journeys@buffalo.edu>
2024-09-19 22:46:01 -0400

    Added some additional stuff to settings modal to make it closer to figma like the sidenav and header. Also added a npm dependency to avoid using deprecated package

--- frontend/src/Page/Settings.jsx
+++ frontend/src/Page/Settings.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { RiCloseLargeLine } from "react-icons/ri";
 const Settings = ({closeSettings}) => {
+    const [showSettings, setShowSettings] = useState('General');
 
 
-  return (
-      <div onClick={closeSettings} className={"settings_modal_bg_container"}>
-        <div onClick={event => event.stopPropagation()} className={"settings_modal_container"}>
-            <div className={"settings_modal_title_band"}>
-                <h2>Settings</h2>
-                <RiCloseLargeLine onClick={closeSettings} size={30} className={"close_modal_button"}/>
+    return (
+        <div onClick={closeSettings} className={"settings_modal_bg_container"}>
+            <div onClick={event => event.stopPropagation()} className={"settings_modal_container"}>
+                <div className={"settings_modal_title_band"}>
+                    <h2>Settings</h2>
+                    <RiCloseLargeLine onClick={closeSettings} size={30} className={"close_modal_button"}/>
+                </div>
+                <div className={"divider_line"}></div>
+                <div className={"settings_modal_content"}>
+                    <div className={"settings_button_sidenav"}>
+                        <button onClick={e => setShowSettings('General')}>General</button>
+                        <button onClick={e => setShowSettings('Security')}>Security</button>
+                    </div>
+                    <div className={"settings_tab_content"}>
+                        {showSettings === 'General' ?
+                            <div>
+                                <div>Change Username</div>
+                                <br/>
+                                <div>Change Profile Picture</div>
+                            </div>:
+                            <div>
+                                <div>Change Password</div>
+                                <br/>
+                                <div>Delete Account</div>
+                            </div>
+                        }
+                    </div>
+                </div>
             </div>
-
-            <div>Hello</div>
         </div>
-      </div>
-  );
+    );
 };
 
 export default Settings;
