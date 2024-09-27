3578182183088336382b5d934328f35c83da5b01
Journey <journeys@buffalo.edu>
2024-09-26 23:23:19 -0400

    Added ChangePFP.jsx file. Not finished.

---
+++ frontend/src/Component/Settings Components/ChangePFP.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const ChangePFP = ( { closeModal, changePFP } ) => {
+
+    return (
+        <div className={"change_background"}>
+        </div>
+    )
+}
+
+export default ChangePFP;
