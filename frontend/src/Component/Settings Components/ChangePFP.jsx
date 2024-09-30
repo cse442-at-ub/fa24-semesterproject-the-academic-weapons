8178ac3170fcea872fed0a3ff4adc125e800d731
Journey <journeys@buffalo.edu>
2024-09-30 15:30:44 -0400

    Implemented change profile picture on the frontend and also revamped the settings modal to make it more mobile friendly - still want to restyle the content display though.

--- frontend/src/Component/Settings Components/ChangePFP.jsx
+++ frontend/src/Component/Settings Components/ChangePFP.jsx
@@ -1,15 +1,28 @@
 import React from "react"
 
 
-const ChangePFP = ( { closeModal, changePFP } ) => {
+const ChangePFP = ( { closeModal, changePFP, pfpMap } ) => {
+
+    const setNewPFP = (e) => {
+        changePFP(e);
+        closeModal();
+    }
 
     return (
         <div onClick={closeModal} className={"change_background"}>
             <div onClick={e => e.stopPropagation()} className={"change_modal_container"}>
                 <div className={"change_modal_title_desc_container"}>
                     <h2>Change Profile Picture</h2>
-                    <div className={"change_modal_desc"}>Choose a new profile picture</div>
+                    <div className={"change_modal_desc"}>Choose a profile picture</div>
                 </div>
+                <div className={"change_pfp_options_container"}>
+                    {Object.values(pfpMap).map((pic, index) =>
+                    <div onClick={e => setNewPFP(index)} className={"profile_picture_container"} key={index}>
+                        <img className={"profile_picture_option"} src={pic} alt={"Profile picture option " + index}/>
+                    </div>
+                    )}
+                </div>
+                <div className={"change_cancel"} onClick={closeModal}>Cancel</div>
             </div>
         </div>
     )
