64ec3a0e06a79ccfeb633c79a7dd3c90909afaf4
Zac Sanger <sangerzachary@gmail.com>
2024-10-03 18:51:28 -0400

    Adjusted for merge with dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/CSE442/2024-Fall/cse-442an/"
-  base: "/CSE442/2024-Fall/zcsanger/"
+  base: "/CSE442/2024-Fall/cse-442an/"
+  // base: "/CSE442/2024-Fall/journeys/"
+  // base: "/CSE442/2024-Fall/zcsanger/"
 })
