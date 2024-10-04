714030e96b6f6752d456615aaa3d12a141d04a1b
matthewlyskawa <mjlyskaw@buffalo.edu>
2024-10-04 02:06:03 -0400

    updated structure of layout.  Maybe this will be how we tackle this

--- frontend/src/Page/Dashboard.jsx
+++ frontend/src/Page/Dashboard.jsx
@@ -6,33 +6,30 @@ import RecentTransactions from '../Component/Dashboard Components/RecentTransact
 import HighestSpending from '../Component/Dashboard Components/HighestSpending';
 import Navbar from "../Component/Navbar.jsx";
 
-const Dashboard = ({openModal, transactions}) => {
+const Dashboard = ({ openModal, transactions }) => {
   return (
     <div className="dashboard">
-            <div className="dashboard-content">
+      <div className="dashboard-content">
         {/*- Pie Chart */}
         <div className="box pie-chart-box">
-        <h2 className="Category_spend_txt">Category Spending</h2>
+          <h2 className="Category_spend_txt">Category Spending</h2>
           <MainPieChart />
-          
         </div>
 
-        {/* - Bar Chart  */}
-        <div className="box bar-chart-box">
-          <BarChartComponent />
+        {/* - Highest Spending Category */}
+        <div className="box highest-spending-box">
+          <HighestSpending />
         </div>
-
 
         {/*  - Recent Transactions  */}
         <div className="box recent-transactions-box">
           <RecentTransactions transactions={transactions} openModal={openModal} />
         </div>
 
-        {/* - Highest Spending Category */}
-        <div className="box highest-spending-box">
-      <HighestSpending />
-      </div>
-
+        {/* - Bar Chart  */}
+        <div className="box bar-chart-box">
+          <BarChartComponent />
+        </div>
       </div>
     </div>
   );
