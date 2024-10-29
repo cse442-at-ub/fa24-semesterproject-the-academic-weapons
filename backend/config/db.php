298b970414d9a7ccc0dfb0c9345d2eb19c03a9e6
Leo <leojay468@gmail.com>
2024-10-29 01:59:16 -0400

    Merge remote-tracking branch 'origin/dev' into Monthly_spending_Frontend. Also added some database things so i can test locally.

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,12 +1,15 @@
 <?php
 
 $servername = "127.0.0.1";
-$username = "mjlyskaw";
-$password = "50393887";
-$dbname = "cse442_2024_fall_team_an_db";
+// $username = "mjlyskaw";
+// $password = "50393887";
+// $dbname = "cse442_2024_fall_team_an_db"; Uncomment before push to dev
 // $username = "journeys";
 // $password = "50400674";
 // $dbname = "journeys_db";
+$username = "root";
+$password = "";
+$dbname = "cse442_2024_fall_team_an_db";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname,);
