91161f0594ed637f8c2a8def84ea759b367581af
journey217 <jspratt217@gmail.com>
2024-12-01 01:48:53 -0500

    Dashboard styles updated to match app to be more consistent

--- frontend/src/Component/Dashboard Components/GraphSelectionModal.jsx
+++ frontend/src/Component/Dashboard Components/GraphSelectionModal.jsx
@@ -9,10 +9,13 @@ const GraphSelectionModal = ({ isOpen, onClose, onSelect }) => {
         <div className="modal-overlay">
             <div className="modal-content">
                 <h3>Select a Chart Type</h3>
-                <button onClick={() => onSelect('pie')}>Pie Chart</button>
-                <button onClick={() => onSelect('bar')}>Bar Chart</button>
-                <button onClick={() => onSelect('line')}>Line Chart</button>
-                <button onClick={onClose}>Close</button>
+                <div className={"select_graph_buttons"}>
+                    <button onClick={() => onSelect('pie')}>Pie Chart</button>
+                    <button onClick={() => onSelect('bar')}>Bar Chart</button>
+                    <button onClick={() => onSelect('line')}>Line Chart</button>
+                    <br/>
+                    <div className={"add_trans_close_text"} onClick={onClose}>Close</div>
+                </div>
             </div>
         </div>
     );
